da2f0d729789033694c21cd6f4b55754251d5c8a
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-09-12 21:14:13 -0300

    subindo codigo

--- array.js
+++ array.js
@@ -6,10 +6,67 @@
 const array = [10,20,30,40,1,2,3];
 
 
+const outroArray = [1,2,3,4,5,6,7]
+
+function contanum (array){
+  let contador = 0
+  for (let index = 0; index < array.length; index++) {
+    
+    let numeroAtual = array[index]
+
+    if(numeroAtual > 10){
+      contador = contador + 1;
+    }
+    
+  }
+  return contador;
+}
+
+console.log(contanum(outroArray))
+
+
 // 2️ Verificar elementos repetidos
 // Crie uma função que receba um array de strings e diga se existe algum elemento repetido.
 
 const strings = ['Ana', 'Bia', 'João', 'Ana'];
+
+
+function verificaStringRepetida (arrayStrings) {
+
+  const aux = {}
+
+  for (let index = 0; index < array.length; index++) {
+
+    let repetiu = false
+    
+    let pessoaAtual = array[index]
+    //Ana
+
+      if (aux[pessoaAtual]) {
+        repetiu = true 
+      }
+
+      aux[pessoaAtual] = true;
+  }
+
+  return repetiu
+
+}
+
+
+// function temRepetido(arr) {
+//   const vistos = {};
+
+//   for (let i = 0; i < arr.length; i++) {
+//     const item = arr[i];
+//     if (vistos[item]) {
+//       return true; // já apareceu antes
+//     }
+//     vistos[item] = true;
+//   }
+
+//   return false;
+// }
 
 // 3 Somar valores de um array de objetos
 // Dado um array de produtos, calcule o preço total.
--- main.js
+++ main.js
@@ -1,43 +1,125 @@
-/* 
-# Prova PW1 - Sistema de Gerenciamento de Tarefas
 
-Nesta prova, você deve implementar um sistema simples de gerenciamento de tarefas utilizando JavaScript. 
-O sistema permitirá a adição, exclusão e conclusão de tarefas, além do cálculo de quantas já foram concluídas.
+// # Prova PW1 - Sistema de Gerenciamento de Tarefas
 
----
-## Questão 01 - Exibição da Lista de Tarefas
-- Crie a função `renderTaskList()` que atualiza a interface com a lista de tarefas.
-- Caso o array `tasks` esteja vazio, exiba a mensagem:
-  <div class="empty-state">Nenhuma tarefa cadastrada...</div>
-- Para cada tarefa, exiba um card com:
-  - Nome da tarefa (`task.name`)
-  - Status da tarefa (pendente ou concluída, visualmente destacado) (task.done)
-  - Botão "Concluir"
-  - Botão "Excluir"
+// Nesta prova, você deve implementar um sistema simples de gerenciamento de tarefas utilizando JavaScript. 
+// O sistema permitirá a adição, exclusão e conclusão de tarefas, além do cálculo de quantas já foram concluídas.
 
----
-## Questão 02 - Cadastro de Tarefas
-- Ao clicar no botão `Adicionar Tarefa`, capture o valor do input `taskName`.
-- Valide se foi informado um nome. Caso contrário, exiba em `.errorMessage`: "Informe o nome da tarefa."
-- Adicione um objeto no array `tasks` com as propriedades:
-  - `name`: nome da tarefa
-  - `done`: false
-- Limpe o campo após adicionar.
-- Atualize a lista chamando `renderTaskList()`.
 
----
-## Questão 03 - Concluir Tarefas
-- No botão "Concluir", altere a propriedade `done` da tarefa correspondente para `true`.
-- Atualize a interface para mostrar a tarefa riscada (classe CSS `done`).
+const tasks = []
 
----
-## Questão 04 - Excluir Tarefas
-- No botão "Excluir", remova a tarefa correspondente do array `tasks`.
-- Atualize a interface chamando `renderTaskList()`.
+// ---
+// ## Questão 01 - Exibição da Lista de Tarefas
+// - Crie a função `renderTaskList()` que atualiza a interface com a lista de tarefas.
+// - Caso o array `tasks` esteja vazio, exiba a mensagem:
+//   <div class="empty-state">Nenhuma tarefa cadastrada...</div>
+// - Para cada tarefa, exiba um card com:
+//   - Nome da tarefa (`task.name`)
+//   - Status da tarefa (pendente ou concluída, visualmente destacado) (task.done)
+//   - Botão "Concluir"
+//   - Botão "Excluir"
 
----
-## Questão 05 - Contar Tarefas Concluídas
-- Ao clicar no botão `Calcular Total Concluídas`, percorra o array `tasks` e conte quantas têm `done: true`.
-- Exiba o resultado em `#countDisplay`.
-- Caso não haja tarefas cadastradas, mostre em `.errorMessage`: "Não é possível calcular sem tarefas."
-*/
+// Função que renderiza a lista na tela
+function renderTaskList() {
+  
+  const list = document.querySelector(".tasks-list")
+  list.innerHTML = "";
+
+  if (tasks.length === 0) {
+    list.innerHTML = `<div class="empty-state">Nenhuma tarefa cadastrada...</div>`;
+    return;
+  }
+
+  tasks.forEach( (element, index) => {
+
+    console.log(element)
+    
+    const card = document.createElement("div");
+    card.classList.add("task-card");
+
+    const span = document.createElement("span");
+    span.classList.add("task-name");
+    span.textContent = element.name;
+    if (element.done == true) {
+      span.classList.add("done");
+    }
+
+    //DELEÇÃO
+    const deleteButton = document.createElement("button");
+    deleteButton.addEventListener('click', () => {
+      tasks.splice(index, 1);
+      renderTaskList();
+    })
+    deleteButton.textContent = "Deletar";
+
+    //CONCLUIR
+    const concButton = document.createElement("button");
+    concButton.addEventListener('click', () => {
+      const tarefaAtual = tasks[index]
+      tarefaAtual.done = true
+      renderTaskList();
+    })
+
+    concButton.textContent = "concluido";
+    card.appendChild(span);
+    card.appendChild(deleteButton);
+    card.appendChild(concButton);
+    list.appendChild(card);
+
+  });
+}
+
+// Eventos principais
+const BotaoAdicionar = document.querySelector("#btnAdd")
+BotaoAdicionar.addEventListener('click', addTask)
+
+const input = document.querySelector("#taskName")
+
+function addTask () {
+  const novoObj = {
+    name: input.value,
+    done:false
+  }
+
+  tasks.push(novoObj)
+
+  input.value = ""
+
+  renderTaskList()
+}
+
+renderTaskList()
+
+
+// ---
+// ## Questão 05 - Contar Tarefas Concluídas
+// - Ao clicar no botão `Calcular Total Concluídas`, percorra o array `tasks` e conte quantas têm `done: true`.
+// - Exiba o resultado em `#countDisplay`.
+// - Caso não haja tarefas cadastradas, mostre em `.errorMessage`: "Não é possível calcular sem tarefas."
+// */
+const calcBtn = document.querySelector("#btnCount");
+const result = document.querySelector("#countDisplay");
+const error = document.querySelector(".errorMessage");
+
+calcBtn.addEventListener('click', () => {
+
+  let cont = 0
+  if (tasks.length === 0) {
+    error.innerHTML = `<div class="empty-state">Nenhuma tarefa cadastrada...</div>`;
+    return;
+  }
+
+  tasks.forEach(element => {
+
+    
+
+    if(element.done == true) {
+      cont++
+    }
+  
+  });
+
+
+  result.textContent = cont
+
+})
+
